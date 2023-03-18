632c1c6e14edbf2ba8a2eea03d9ca9b41f8a4254
Ava Gordon <avagordon01@gmail.com>
2023-03-18 14:04:47 +0000

    remove leb128.cc

--- include/leb128.hh
+++ include/leb128.hh
@@ -6,9 +6,17 @@
 #include <limits>
 #include <stdexcept>
 
-uint8_t high_bit(uint8_t x);
-uint8_t low_bits(uint8_t x);
-uint8_t sign_bit(uint8_t x);
+namespace {
+    uint8_t high_bit(uint8_t x) {
+        return x & 0x80;
+    }
+    uint8_t low_bits(uint8_t x) {
+        return x & 0x7f;
+    }
+    uint8_t sign_bit(uint8_t x) {
+        return x & 0x40;
+    }
+}
 
 struct uleb128 {
     uint64_t data;
